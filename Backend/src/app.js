09401790342dae4241212abec7f12b40dffabc8e
shubham <shubhamsagar628@gmail.com>
2024-10-01 12:02:01 +0530

    Explore Multiple Ways of Routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,17 @@ const express = require('express')
 const app = express() 
 
 
-app.use("/test",(req,res)=>{
+app.get("/test/:id",(req,res)=>{
+    console.log(req.params)
+
+})
+
+app.get("/man",(req,res)=>{
+    console.log(req.query)
+    res.send("Hello From Test World")
+})
+
+app.get("/test*c",(req,res)=>{
     res.send("Hello From Test World")
 })
 
@@ -15,6 +25,12 @@ app.use("/car",(req,res)=>{
 app.use("/animal",(req,res)=>{
     res.send("Hello from the animal ")
 })
+
+
+app.use("/",(req,res)=>{
+    res.send("This is Home Path")
+})
+
 
 app.listen(7777,()=>{
     console.log("Server is listening on Port No 7777")
