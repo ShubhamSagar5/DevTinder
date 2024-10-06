87514992917656fc5cf4b95379e4bbd07f25e90b
shubham <shubhamsagar628@gmail.com>
2024-10-06 21:39:48 +0530

    Feed Api and Pagination

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { userAuth } = require('../middleware/auth')
 const { ConnectionRequestModel } = require('../models/ConnectionRequest')
+const { User } = require('../models/User')
 
 const userRouter = express.Router() 
 
-
+const USE_SAFE_DATA = "firstName lastName age gender photoUrl about skills"
 userRouter.get("/user/request/recived",userAuth,async(req,res)=>{
     try {
         
@@ -33,7 +34,7 @@ userRouter.get("/user/connection/list",userAuth,async(req,res)=>{
     try {
         const loggedInUser = req.user
 
-        const USE_SAFE_DATA = "firstName lastName age gender photoUrl about skills"
+      
 
         const connectionRequest = await ConnectionRequestModel.find({
             $or:[
@@ -62,5 +63,54 @@ userRouter.get("/user/connection/list",userAuth,async(req,res)=>{
         })
     }
 })
+
+
+userRouter.get("/user/feed",userAuth,async(req,res)=>{
+    try {
+        
+        const loggedInUser = req.user 
+
+        const page = parseInt(req.query.page) || 1 
+        let limit = parseInt(req.query.limit) || 10 
+
+        limit = limit > 50 ? 50 : limit
+
+        const skip = (page-1) * limit
+
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or:[
+                {fromUserId:loggedInUser._id},
+                {toUserId:loggedInUser._id}
+            ]
+        })
+
+        const hideUserFromFeed = new Set() 
+
+        connectionRequest.forEach((req)=>{
+            hideUserFromFeed.add(req.fromUserId.toString())
+            hideUserFromFeed.add(req.toUserId.toString())
+        })
+
+        const user = await User.find({
+            $and:[
+                {_id:{$nin:Array.from(hideUserFromFeed)}},
+                {_id:{$ne:loggedInUser._id}}
+            ]
+        }).select(USE_SAFE_DATA).skip(skip).limit(limit)
+
+
+
+
+        return res.status(200).json({
+            success:true,
+            data:user
+        })
+
+
+    } catch (error) {
+        
+    }
+})
+
 
 module.exports = userRouter
